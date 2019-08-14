22c8f7849db71104ab95769f88d8d3302783c675
Povilas Juzeliunas <pju@trifork.com>
2019-08-14 11:38:51 +0200

    Allowing subclassing of AlertController

--- Source/AlertController.swift
+++ Source/AlertController.swift
@@ -22,7 +22,7 @@ public enum ActionLayout: Int {
 }
 
 @objc(SDCAlertController)
-public final class AlertController: UIViewController {
+public class AlertController: UIViewController {
     private var verticalCenter: NSLayoutConstraint?
 
     /// The alert's title. Directly uses `attributedTitle` without any attributes.
